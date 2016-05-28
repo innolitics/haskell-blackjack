04512aabfb17d1c9295ebacf174b0bdaa38e79c7
J. David Giese <johndgiese@gmail.com>
2016-05-28 00:48:06 -0600

    Clarify and simplify
    - Rename denomination to rank
    - Improve rank data constructors
    - Improve show function for card
    - Use pipe to default the 10 score case
    - Move show stuff to the bottom of the file bc it is less relevant

--- Main.hs
+++ Main.hs
@@ -1,56 +1,36 @@
 data Suite = Spades | Hearts | Diamonds | Clubs
 
+data Rank = R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 | R10 | RJ | RQ | RK | RA deriving Show
+
+data Card = Card { rank :: Rank, suite :: Suite }
+
+type Hand = [Card]
+
+scoreHand :: Hand -> [Int]
+scoreHand h = map sum $ sequence $ map scoreCard h
+
+scoreCard :: Card -> [Int]
+scoreCard (Card d s) = scoreRank d
+
+scoreRank :: Rank -> [Int]
+scoreRank R2 = [2]
+scoreRank R3 = [3]
+scoreRank R4 = [4]
+scoreRank R5 = [5]
+scoreRank R6 = [6]
+scoreRank R7 = [7]
+scoreRank R8 = [8]
+scoreRank R9 = [9]
+scoreRank RA = [1, 11]
+scoreRank _  = [10]
+
+main = putStrLn $ show $ scoreHand [Card R3 Spades, Card RA Spades]
+
 instance Show Suite where
     show Spades = "♠"
     show Hearts = "♥"
     show Diamonds = "♦"
     show Clubs = "♣"
 
-data Denomination =
-      Two
-    | Three
-    | Four
-    | Five
-    | Six
-    | Seven
-    | Eight
-    | Nine
-    | Ten
-    | Jack
-    | Queen
-    | King
-    | Ace
-    deriving Show
-
-data Card = Card {
-    denomination :: Denomination,
-    suite :: Suite
-}
-
 instance Show Card where
-    show (Card d s) = show d ++ (show s)
-
-type Hand = [Card]
-
-handScore :: Hand -> [Int]
-handScore h = map sum $ sequence $ map cardScore h
-
-cardScore :: Card -> [Int]
-cardScore (Card d s) = denominationScore d
-
-denominationScore :: Denomination -> [Int]
-denominationScore Two = [2]
-denominationScore Three = [3]
-denominationScore Four = [4]
-denominationScore Five = [5]
-denominationScore Six = [6]
-denominationScore Seven = [7]
-denominationScore Eight = [8]
-denominationScore Nine = [9]
-denominationScore Ten = [10]
-denominationScore Jack = [10]
-denominationScore Queen = [10]
-denominationScore King = [10]
-denominationScore Ace = [1, 11]
-
-main = putStrLn $ show $ handScore [Card Three Spades, Card Ace Spades]
+    show (Card d s) = tail $ show d ++ show s
